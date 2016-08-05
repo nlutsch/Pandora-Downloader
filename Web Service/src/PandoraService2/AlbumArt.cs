dc44b1dcbaca223e5f14e8b4d6ca9924b9975373
Nick <lutschn1@central.edu>
2016-08-05 09:53:06 -0500

    Added ID3 data, including Album Art

---
+++ Web Service/src/PandoraService2/AlbumArt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using TagLib;
+
+namespace PandoraService2
+{
+    public class AlbumArt : IPicture
+    {
+        public ByteVector Data { get; set; }
+        public string Description { get; set; }
+        public string MimeType { get; set; }
+        public PictureType Type { get; set; }
+
+        public AlbumArt(Stream stream)
+        {
+            Data = ByteVector.FromStream(stream);
+            Description = "AlbumArt";
+            MimeType = "image/jpeg";
+            Type = PictureType.FrontCover;
+        }
+
+    }
+}
--- Web Service/src/PandoraService2/Controllers/DownloadController.cs
+++ Web Service/src/PandoraService2/Controllers/DownloadController.cs
@@ -24,38 +24,64 @@ namespace PandoraService2.Controllers
         public IActionResult Download(string title, string artist, string album, string station, string url, string artUrl)
         {
             var uri = new Uri(url);
-            var filename = artist + " - " + title + ".mp3";
+            var artUri = new Uri(artUrl);
+            var songfilename = station + "/" + artist + " - " + title + ".mp3";
+            var artfilename = station + "/artwork/" + artist + " - " + title + ".jpg";
+
+            Directory.CreateDirectory(station);
+            Directory.CreateDirectory(station + "/artwork");
 
             //Microsoft.Extensions.Logging.Console.
             _logger.LogInformation("Attempting to download '{0}' by '{1}'.", title, artist);
 
-            DownloadAsync(uri, filename, title, artist, album);
+            DownloadAsync(uri, songfilename, title, artist, album, artUri, artfilename);
             
             return Ok();
         }
 
-        private async Task DownloadAsync(Uri uri, string filename, string title, string artist, string album)
+        private async Task DownloadAsync(Uri uri, string songfilename, string title, string artist, string album, Uri artUri, string artfilename)
         {
+            AlbumArt objArt = null;
             using (var httpClient = new HttpClient())
             {
                 using (var request = new HttpRequestMessage(HttpMethod.Get, uri))
                 {
                     using (
                         Stream contentStream = await (await httpClient.SendAsync(request)).Content.ReadAsStreamAsync(),
-                        stream = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.None, 10000000, true))
+                        stream = new FileStream(songfilename, FileMode.Create, FileAccess.Write, FileShare.None, 10000000, true))
                     {
                         await contentStream.CopyToAsync(stream);
-                        _logger.LogInformation("Successfully Downloaded {0}!", filename);
-
-                        using (var tagFile = TagLib.File.Create(new StreamFileAbstraction(filename, stream, stream)))
-                        {
-                            tagFile.Tag.Title = title;
-                            tagFile.Tag.Performers = new[] { artist };
-                            tagFile.Tag.Album = album;
-                            tagFile.Save();
-                        }
+                        _logger.LogInformation("Successfully Downloaded {0}!", songfilename);
                     }
                 }
+
+                using (var request = new HttpRequestMessage(HttpMethod.Get,artUri))
+                {
+                    using (
+                        Stream contentStream = await (await httpClient.SendAsync(request)).Content.ReadAsStreamAsync(),
+                        stream = new FileStream(artfilename, FileMode.Create, FileAccess.Write, FileShare.None, 100000, true))
+                    {
+                        await contentStream.CopyToAsync(stream);                        
+                    }
+                }
+
+                using (var stream = new FileStream(artfilename, FileMode.Open, FileAccess.ReadWrite))
+                {
+                    objArt = new AlbumArt(stream);
+                }
+
+                using (var stream = new FileStream(songfilename, FileMode.Open, FileAccess.ReadWrite))
+                {
+                    using (var tagFile = TagLib.File.Create(new StreamFileAbstraction(songfilename, stream, stream)))
+                    {
+                        tagFile.Tag.Title = title;
+                        tagFile.Tag.Performers = new[] { artist };
+                        tagFile.Tag.Album = album;
+                        tagFile.Tag.Pictures = new IPicture[] { objArt };
+                        tagFile.Save();
+                    }
+                }
+
             }
         }
 
