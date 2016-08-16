8d01c4faba74874167167531502f20a366bd53e5
Nick <lutschn1@central.edu>
2016-08-16 15:01:15 -0500

    Updated Album Art download exception handling

--- Web Service/src/PandoraService2/Controllers/DownloadController.cs
+++ Web Service/src/PandoraService2/Controllers/DownloadController.cs
@@ -27,7 +27,6 @@ namespace PandoraService2.Controllers
         public IActionResult Download(string title, string artist, string album, string station, string url, string artUrl)
         {
             var uri = new Uri(url);
-            var artUri = new Uri(artUrl);
             var songfilename = _settings.DownloadLocation + "/" + station + "/" + artist + " - " + title + ".mp3";
             var artfilename = _settings.DownloadLocation + "/" + station + "/artwork/" + album + ".jpg";
 
@@ -49,12 +48,12 @@ namespace PandoraService2.Controllers
                 return Ok("Already Exists");
             }
 
-            DownloadAsync(uri, songfilename, title, artist, album, artUri, artfilename);
+            DownloadAsync(uri, songfilename, title, artist, album, artUrl, artfilename);
             
             return Ok();
         }
 
-        private async Task DownloadAsync(Uri uri, string songfilename, string title, string artist, string album, Uri artUri, string artfilename)
+        private async Task DownloadAsync(Uri uri, string songfilename, string title, string artist, string album, string artUrl, string artfilename)
         {
             AlbumArt objArt = null;
             using (var httpClient = new HttpClient())
@@ -70,19 +69,27 @@ namespace PandoraService2.Controllers
                     }
                 }
 
-                using (var request = new HttpRequestMessage(HttpMethod.Get,artUri))
+                try
                 {
-                    using (
-                        Stream contentStream = await (await httpClient.SendAsync(request)).Content.ReadAsStreamAsync(),
-                        stream = new FileStream(artfilename, FileMode.Create, FileAccess.Write, FileShare.None, 100000, true))
+                    using (var request = new HttpRequestMessage(HttpMethod.Get, new Uri(artUrl)))
                     {
-                        await contentStream.CopyToAsync(stream);                        
+                        using (
+                            Stream contentStream = await (await httpClient.SendAsync(request)).Content.ReadAsStreamAsync(),
+                            stream = new FileStream(artfilename, FileMode.Create, FileAccess.Write, FileShare.None, 100000, true))
+                        {
+                            await contentStream.CopyToAsync(stream);
+                        }
+                    }
+
+                    using (var stream = new FileStream(artfilename, FileMode.Open, FileAccess.ReadWrite))
+                    {
+                        objArt = new AlbumArt(stream);
                     }
                 }
-
-                using (var stream = new FileStream(artfilename, FileMode.Open, FileAccess.ReadWrite))
+                
+                catch
                 {
-                    objArt = new AlbumArt(stream);
+                    _logger.LogInformation("Failed to download artwork", songfilename);
                 }
 
                 using (var stream = new FileStream(songfilename, FileMode.Open, FileAccess.ReadWrite))
@@ -92,7 +99,10 @@ namespace PandoraService2.Controllers
                         tagFile.Tag.Title = title;
                         tagFile.Tag.Performers = new[] { artist };
                         tagFile.Tag.Album = album;
-                        tagFile.Tag.Pictures = new IPicture[] { objArt };
+                        if (objArt != null)
+                        {
+                            tagFile.Tag.Pictures = new IPicture[] { objArt };
+                        }                        
                         tagFile.Save();
                     }
                 }
